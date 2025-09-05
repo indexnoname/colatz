6abe8d5d6ff3202866ff4a5a9b6241df16ee554c
indexnoname <indexnoname@proton.me>
2025-09-04 20:16:49 -0700

    major improvement by using caching, however i am not sure if it is working + I think I need to minimize the memory footpring smh. tho performance has increased a lot,

--- colatz/colatz.cpp
+++ colatz/colatz.cpp
@@ -5,7 +5,78 @@
 
 using namespace boost::multiprecision;
 
-const static uint64_t PRINT_VALUE = (1ULL << 25) - 1;
+constexpr static uint64_t PRINT_VALUE = (1ULL << 31) - 1;
+
+
+
+constexpr static uint64_t K = 14; // set this up depending on your cpu cache size (prob depending on L1 and L2 cache)
+constexpr static uint64_t SIZE = 1 << K;
+
+std::array<uint8_t, SIZE> c_table;
+std::array<uint64_t, SIZE> d_table;
+
+static std::array<uint64_t, K> power_three;
+
+void init_tables() {
+    for (int b = 0; b < SIZE; b++) {
+        uint8_t c = 0;
+        uint64_t x = b;
+        for (uint16_t step = 0; step < K; step++) {
+            if (x & 1) {
+                x = (3 * x + 1) >> 1;
+                c++;
+            }
+            else {
+                x >>= 1;
+            }
+        }
+        c_table[b] = c;
+        d_table[b] = x;
+    }
+    uint64_t p = 1;
+    for (int i = 0; i < 64; ++i) {
+        power_three[i] = p;
+        p *= 3;
+    }
+}
+
+void collatzSequence(uint64_t start_val) {
+    uint64_t i = start_val;
+    while (i >= start_val) {
+        uint64_t a = i >> K;
+        int b = static_cast<int>(i & (SIZE - 1));
+
+        i = a * power_three[c_table[b]] + d_table[b];
+    }
+}
+
+int main() {
+    uint64_t start = 3;
+    init_tables();
+    while (true) {
+        collatzSequence(start);
+        start += 4;
+        if ((start & PRINT_VALUE) == 3) std::cout << start << std::endl;
+    }
+    return 0;
+}
+
+// latest 61499636711427
+// YO LETS GO billion throughput in a second
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+old code, eh it was good one, bye bye bro
 
 static std::array<cpp_int, 64> power_three;
 static std::array<cpp_int, 64> sum_three;
@@ -15,9 +86,8 @@ void init_tables() {
     cpp_int s = 0;
     for (int i = 0; i < 64; ++i) {
         power_three[i] = p;
-        sum_three[i] = s;
+        sum_three[i] = p >> 1;
         p *= 3;
-        s = (p - 1) / 2;
     }
 }
 
@@ -46,3 +116,4 @@ int main() {
 // latest 8329015328771
 // latest 9687298736129
 // latest 10475962105857
+*/
